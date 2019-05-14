e98cf576dbe024be3cffaaf8e169bf2bbc951bab
Riken Mehta <riken.mehta03@gmail.com>
2019-05-14 14:40:41 -0400

    [UPDATE]shell script

--- ai_playground/sac/run_training.sh
+++ ai_playground/sac/run_training.sh
@@ -7,7 +7,7 @@ for SEED in 1 2 3 4 5 6 7 8 9 10; do
   SUBDIR=seed_${SEED}
   SAVEDIR=${CDIR}/${SUBDIR}
   mkdir -p ${SAVEDIR}
-  screen -dmS ${SUBDIR} source ~/movo/movo-env/bin/activate && python train.py \
+  screen -dmS ${SUBDIR} bash -c 'source ~/movo/movo-env/bin/activate; python train.py \
     --eval_freq 10000 \
     --start_steps 10000 \
     --max_steps 10000000 \
@@ -15,5 +15,5 @@ for SEED in 1 2 3 4 5 6 7 8 9 10; do
     --batch_size 1024  \
     --save_dir ${SAVEDIR} \
     --seed ${SEED} \
-    --use_tb
+    --use_tb'
 done
